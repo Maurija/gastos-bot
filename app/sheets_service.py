c919a46aa9d4f121921a006649d766a9f8808815
Mauro Evangelista <mevangelista@senasa.gov.ar>
2026-02-26 15:30:53 -0300

    fix: dejar de usar el archivo credential.json para usar las variables de entorno de Render

--- app/sheets_service.py
+++ app/sheets_service.py
@@ -10,8 +10,16 @@ SCOPES = [
 SPREADSHEET_ID = "1I4UGI6rW0tG0kYIhkrAMqtr5Z8X7LKM_IqiSirsXGEo"
 
 def get_sheet():
-    creds = Credentials.from_service_account_file(
-        "app/credentials.json",
+    # 🔥 Leer credenciales desde variable de entorno
+    creds_json = os.getenv("GOOGLE_CREDENTIALS")
+
+    if not creds_json:
+        raise Exception("No se encontró GOOGLE_CREDENTIALS en las variables de entorno")
+
+    creds_dict = json.loads(creds_json)
+
+    creds = Credentials.from_service_account_info(
+        creds_dict,
         scopes=SCOPES
     )
 
